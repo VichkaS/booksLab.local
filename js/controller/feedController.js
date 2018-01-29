023243ea1dba41a43924914370655a3b8b8240e6
victoria_gavrilenko <vi4ka.vs@gmail.com>
2018-01-29 21:03:07 +0300

    Отображение всех книг с правильными статусами

--- js/controller/feedController.js
+++ js/controller/feedController.js
@@ -10,8 +10,59 @@ angular.module('BooksApp').controller("feedController", ['$scope', '$http', func
           url: "http://localhost:3000/books",
           dataType: "json" }).then(
           function successCallback(response) {
-              $scope.listBooks = response.data.books;
-              console.log(response);
+              
+              if(token == '') {
+                  $scope.listBooks = response.data.books;
+              } else {
+                  var list = response.data.books;
+                  for (var i = 0; i < list.length; i++) {
+                      list[i].favorite = false;
+                      list[i].read = false;
+                  }
+                  
+                  var readBooks = [];
+                  var favoriteBooks = [];
+                  $http({
+                      method: "GET",
+                      url: "http://localhost:3000/books/reads",
+                      headers: {"x-access-token": token},
+                      dataType: "json" }).then(
+                      function successCallback(response) {
+                          readBooks = response.data.books;
+                          console.log(readBooks);
+                   });
+                  
+                   $http({
+                      method: "GET",
+                      url: "http://localhost:3000/books/favorites",
+                      headers: {"x-access-token": token},
+                      dataType: "json" }).then(
+                      function successCallback(response) {
+                          favoriteBooks = response.data.books;
+                          console.log(favoriteBooks);
+ 
+                          for (var i = 0; i < list.length; i++) {
+                              console.log(list[i]);
+                              if (readBooks.length > 0) {
+                                  for (var j = 0; j < readBooks.length; j++) {
+                                      if (list[i]._id == readBooks[j]._id) {
+                                          list[i].read = readBooks[j].read;
+                                      }
+                                  };
+                               }
+
+                              for (var k = 0; k < favoriteBooks.length; k++) {
+                                  if (list[i]._id === favoriteBooks[k]._id) {
+                                      list[i].favorite = favoriteBooks[k].favorite;
+                                      console.log(favoriteBooks[k]);
+                                  }
+                              };
+                          }
+
+                          $scope.listBooks = list;
+                          
+                  });
+              }
           });
     }
     
@@ -88,6 +139,7 @@ angular.module('BooksApp').controller("feedController", ['$scope', '$http', func
               $scope.logInEmail = "";
               $scope.logInPass = "";
               $scope.loginError = false;
+              $scope.getBooks();
               console.log(token);
           }, function error(response) {
               $scope.loginError = true;
@@ -162,11 +214,8 @@ angular.module('BooksApp').controller("feedController", ['$scope', '$http', func
           headers: {"x-access-token": token},
           dataType: "json" }).then(
           function successCallback(response) {
-              for (var i = 0; i < $scope.listBooks.length; i++) {
-                if ($scope.listBooks[i]._id == id) {
-                    $scope.listBooks[i].favorite = !$scope.listBooks[i].favorite;
-                 }
-              }
+              $scope.getFavoritesBooks();
+
           }, function error(response) {
               console.log(response);
           });
@@ -179,7 +228,7 @@ angular.module('BooksApp').controller("feedController", ['$scope', '$http', func
           headers: {"x-access-token": token},
           dataType: "json" }).then(
           function successCallback(response) {
-              console.log(token);
+              $scope.getReadBooks();
           }, function error(response) {
               console.log(response);
           });
